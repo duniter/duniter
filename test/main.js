337d7bbd2ef0cf9468f9e1a18b1e77d234da4223
cgeek <cem.moreau@gmail.com>
2013-08-19 11:36:21 +0200

    Integration testing: PKS, memberships, community/memberships

--- test/main.js
+++ test/main.js
@@ -78,10 +78,16 @@ var pubkeyCatSig  = fs.readFileSync(__dirname + '/data/lolcat.pub.asc', 'utf8');
 var pubkeyTobi    = fs.readFileSync(__dirname + '/data/uchiha.pub', 'utf8');
 var pubkeyTobiSig = fs.readFileSync(__dirname + '/data/uchiha.pub.asc', 'utf8');
 
+var joinSnow = fs.readFileSync(__dirname + '/data/membership/snow.join', 'utf8');
+var joinCat  = fs.readFileSync(__dirname + '/data/membership/lolcat.join', 'utf8');
+var joinTobi = fs.readFileSync(__dirname + '/data/membership/tobi.join', 'utf8');
+
 var app;
 var apiRes = {
   '/pks/add': [],
-  '/pks/lookup?op=index&search=': []
+  '/pks/lookup?op=index&search=': [],
+  '/hdc/community/join': [],
+  '/hdc/community/memberships': []
 };
 before(function (done) {
   async.waterfall([
@@ -195,6 +201,95 @@ before(function (done) {
           next();
         });
     },
+    function (next) {
+      var url = '/hdc/community/memberships';
+      request(app)
+        .get(url)
+        .end(function (err, res) {
+          apiRes[url].push({
+            res: res
+          });
+          next();
+        });
+    },
+    function (next) {
+      var url = '/hdc/community/join';
+      request(app)
+        .post(url)
+        .send({
+          "request": joinSnow.substr(0, joinSnow.indexOf('-----BEGIN')),
+          "signature": joinSnow.substr(joinSnow.indexOf('-----BEGIN'))
+        })
+        .end(function (err, res) {
+          apiRes[url].push({
+            res: res
+          });
+          next();
+        });
+    },
+    function (next) {
+      var url = '/hdc/community/memberships';
+      request(app)
+        .get(url)
+        .end(function (err, res) {
+          apiRes[url].push({
+            res: res
+          });
+          next();
+        });
+    },
+    function (next) {
+      var url = '/hdc/community/join';
+      request(app)
+        .post(url)
+        .send({
+          "request": joinTobi.substr(0, joinTobi.indexOf('-----BEGIN')),
+          "signature": joinTobi.substr(joinTobi.indexOf('-----BEGIN'))
+        })
+        .end(function (err, res) {
+          apiRes[url].push({
+            res: res
+          });
+          next();
+        });
+    },
+    function (next) {
+      var url = '/hdc/community/memberships';
+      request(app)
+        .get(url)
+        .end(function (err, res) {
+          apiRes[url].push({
+            res: res
+          });
+          next();
+        });
+    },
+    function (next) {
+      var url = '/hdc/community/join';
+      request(app)
+        .post(url)
+        .send({
+          "request": joinCat.substr(0, joinCat.indexOf('-----BEGIN')),
+          "signature": joinCat.substr(joinCat.indexOf('-----BEGIN'))
+        })
+        .end(function (err, res) {
+          apiRes[url].push({
+            res: res
+          });
+          next();
+        });
+    },
+    function (next) {
+      var url = '/hdc/community/memberships';
+      request(app)
+        .get(url)
+        .end(function (err, res) {
+          apiRes[url].push({
+            res: res
+          });
+          next();
+        });
+    },
   ], function (err) {
     console.log("API fed.");
     done(err);
@@ -203,19 +298,92 @@ before(function (done) {
 
 //----------- PKS -----------
 describe('Sending public key', function(){
+  var index = -1;
+  var url = '/pks/add';
   it('of John Snow should respond 200', function(){
-    apiRes['/pks/add'][0].res.should.have.status(200);
+    apiRes[url][++index].res.should.have.status(200);
+    isPubKey(JSON.parse(apiRes[url][index].res.text));
   });
   it('of LoL Cat should respond 200', function(){
-    apiRes['/pks/add'][1].res.should.have.status(200);
+    apiRes[url][++index].res.should.have.status(200);
+    isPubKey(JSON.parse(apiRes[url][index].res.text));
   });
   it('of Tobi Uchiha should respond 200', function(){
-    apiRes['/pks/add'][2].res.should.have.status(200);
+    apiRes[url][++index].res.should.have.status(200);
+    isPubKey(JSON.parse(apiRes[url][index].res.text));
   });
   it('of Tobi Uchiha with signature of John Snow should respond 400', function(){
-    apiRes['/pks/add'][3].res.should.have.status(400);
+    apiRes[url][++index].res.should.have.status(400);
   });
 });
+
+describe('Sending membership', function(){
+  var index = -1;
+  var url = '/hdc/community/join';
+  it('of John Snow should respond 200', function(){
+    var json = JSON.parse(apiRes[url][++index].res.text);
+    json.should.have.property('request');
+    json.should.have.property('signature');
+  });
+  it('of LoL Cat should respond 200', function(){
+    var json = JSON.parse(apiRes[url][++index].res.text);
+    json.should.have.property('request');
+    json.should.have.property('signature');
+  });
+  it('of Tobi Uchiha should respond 200', function(){
+    var json = JSON.parse(apiRes[url][++index].res.text);
+    json.should.have.property('request');
+    json.should.have.property('signature');
+  });
+  var index2 = -1;
+  var url2 = '/hdc/community/memberships';
+  it('- Merkle root should be ""', function(){
+    var json = JSON.parse(apiRes[url2][++index2].res.text);
+    isMerkleNodesResult(json);
+    json.merkle.levels[0].nodes.length.should.equal(0);
+  });
+  it('- Merkle root should be 0FBA64435A87B7B7CBA2A914A79EB015DD246ECB', function(){
+    var json = JSON.parse(apiRes[url2][++index2].res.text);
+    isMerkleNodesResult(json);
+    json.merkle.levels[0].nodes[0].should.equal('0FBA64435A87B7B7CBA2A914A79EB015DD246ECB');
+  });
+  it('- Merkle root should be C00DCEB6F1B00D4C0CADCC9E35011C50DE2549AB', function(){
+    var json = JSON.parse(apiRes[url2][++index2].res.text);
+    isMerkleNodesResult(json);
+    json.merkle.levels[0].nodes[0].should.equal('C00DCEB6F1B00D4C0CADCC9E35011C50DE2549AB');
+  });
+  it('- Merkle root should be 2A42C5CCC315AF3B9D009CC8E635F8492111F91D', function(){
+    var json = JSON.parse(apiRes[url2][++index2].res.text);
+    isMerkleNodesResult(json);
+    json.merkle.levels[0].nodes[0].should.equal('2A42C5CCC315AF3B9D009CC8E635F8492111F91D');
+  });
+});
+
+function isMerkleNodesResult (json) {
+  isMerkleResult(json);
+  json.merkle.should.have.property('levels');
+}
+
+function isMerkleLeavesResult (json) {
+  isMerkleResult(json);
+  json.merkle.should.have.property('leaves');
+}
+
+function isMerkleResult (json) {
+  json.should.have.property('merkle');
+  json.merkle.should.have.property('depth');
+  json.merkle.should.have.property('nodesCount');
+  json.merkle.should.have.property('levelsCount');
+}
+
+function isPubKey (json) {
+  json.should.have.property('email');
+  json.should.have.property('name');
+  json.should.have.property('fingerprint');
+  json.should.have.property('raw');
+  json.should.not.have.property('_id');
+  json.raw.should.not.match(/-----/g);
+}
 
 // for (var i = 0; i < gets.length; i++) {
 //   testGET(gets[i].url, gets[i].expect);

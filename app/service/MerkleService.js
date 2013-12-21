c611c21505cdb88008442ab9295484068c64b2cf
cgeek <cem.moreau@gmail.com>
2013-12-21 19:09:59 +0100

    Changed Merkle results to fit HTTP API changes

--- app/service/MerkleService.js
+++ app/service/MerkleService.js
@@ -10,42 +10,26 @@ module.exports = {
   },
 
   processForURL: function (req, merkle, valueCB, done) {
-    // Level
-    var lstart = req.query.lstart ? parseInt(req.query.lstart) : 0;
-    var lend   = req.query.lend ? parseInt(req.query.lend) : lstart + 1;
-    if(req.query.extract){
-      lstart = merkle.depth;
-      lend = lstart + 1;
-    }
-    // Start
-    var start = req.query.start ? parseInt(req.query.start) : 0;
-    // End
-    var end = req.query.end ? parseInt(req.query.end) : merkle.levels[merkle.depth.length];
     // Result
     var json = {
       "depth": merkle.depth,
       "nodesCount": merkle.nodes,
-      "levelsCount": merkle.levels.length,
-      "leavesCount": merkle.levels[merkle.depth].length
+      "leavesCount": merkle.levels[merkle.depth].length,
+      "root": merkle.levels[0][0] || ""
     };
-    if(isNaN(lstart)) lstart = 0;
-    if(isNaN(lend)) lend = lstart + 1;
-    if(isNaN(start)) start = 0;
-    if(!req.query.extract || !valueCB){
-      json.levels = {};
-      for (var i = Math.max(lstart, 0); i < merkle.levels.length && i < lend; i++) {
-        var rowEnd = isNaN(end) ? merkle.levels[i].length : end;
-        json.levels[i] = merkle.levels[i].slice(Math.max(start, 0), Math.min(rowEnd, merkle.levels[i].length));
-      };
+    if(!req.query.leaf || !valueCB){
+      // Leaves
+      json.leaves = merkle.leaves();
       done(null, json);
-    }
-    else {
+    } else {
+      // Extract of a leaf
       json.leaves = {};
       var rowEnd = isNaN(end) ? merkle.levels[merkle.depth].length : end;
-      var hashes = merkle.levels[merkle.depth].slice(Math.max(start, 0), Math.min(rowEnd, merkle.levels[lstart].length));
+      var hashes = [req.query.leaf];
+      // This code is in a loop for historic reasons. Should be set to non-loop style.
       valueCB(hashes, function (err, values) {
         hashes.forEach(function (hash, index){
-          json.leaves[Math.max(start, 0) + index] = {
+          json.leaf = {
             "hash": hash,
             "value": values[hash] || ""
           };

2eacc44566cb241465899fb64f9760623c1f089e
cgeek <cem.moreau@gmail.com>
2014-09-11 00:28:29 +0200

    Fix: wrong signature format test (was too restrictive)

--- app/lib/constants.js
+++ app/lib/constants.js
@@ -13,7 +13,7 @@ module.exports = {
   UDID2_FORMAT: /\(udid2;c;([A-Z-]*);([A-Z-]*);(\d{4}-\d{2}-\d{2});(e\+\d{2}\.\d{2}(\+|-)\d{3}\.\d{2});(\d+)(;?)\)/,
   BASE58: /^[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]+$/,
   PUBLIC_KEY: /^[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{44}$/,
-  SIG: /^[A-Za-z0-9-_=]{87}$/,
+  SIG: /^[A-Za-z0-9+\/=]{87,88}$/,
   CERT: {
     SELF: {
       UID: /^UID:udid2;c;([A-Z-]*);([A-Z-]*);(\d{4}-\d{2}-\d{2});(e\+\d{2}\.\d{2}(\+|-)\d{3}\.\d{2});(\d+)(;?)$/,
@@ -21,7 +21,7 @@ module.exports = {
     },
     OTHER: {
       META: META_TS,
-      INLINE: /^[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{44}:[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{44}:[1-9][0-9]*:[A-Za-z0-9-_=]{87}$/
+      INLINE: /^[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{43,44}:[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{43,44}:[1-9][0-9]*:[A-Za-z0-9+\/=]{87,88}$/
     }
   }
 };

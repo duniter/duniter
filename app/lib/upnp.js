6a7a6a1e093d7f56e16508e745a6407194e6619a
cgeek <cem.moreau@gmail.com>
2015-09-27 11:44:12 +0200

    Fix: UPnP error handling changed

--- app/lib/upnp.js
+++ app/lib/upnp.js
@@ -9,18 +9,22 @@ module.exports = function (localPort, remotePort) {
   "use strict";
   return co(function *() {
     logger.info('UPnP: configuring...');
-    var client = upnp.createClient();
-    yield Q.nbind(client.externalIp, client)()
-      .catch(function(err){
-        if (err && err.message == 'timeout') {
-          throw 'No UPnP gateway found: your node won\'t be reachable from the Internet. Use --noupnp option to avoid this message.'
-        }
-        throw err;
-      });
-    client.close();
     // Update UPnP IGD every INTERVAL seconds
     setInterval(async.apply(openPort, localPort, remotePort), 1000 * constants.NETWORK.UPNP.INTERVAL);
-    return openPort(localPort, remotePort);
+    return openPort(localPort, remotePort)
+      .catch(function(){
+        var client = upnp.createClient();
+        return Q.nbind(client.externalIp, client)()
+          .catch(function(err){
+            if (err && err.message == 'timeout') {
+              throw 'No UPnP gateway found: your node won\'t be reachable from the Internet. Use --noupnp option to avoid this message.'
+            }
+            throw err;
+          })
+          .finally(function() {
+            client.close();
+          });
+      });
   });
 };
 
@@ -36,6 +40,7 @@ function openPort (localPort, remotePort) {
     }, function(err) {
       client.close();
       if (err) {
+        logger.warn(err);
         return reject(err);
       }
       resolve();

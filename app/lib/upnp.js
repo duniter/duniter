8cf4ef0afe6b094f5c52575c631fd1c330fcccbf
cgeek <cem.moreau@gmail.com>
2015-09-27 10:06:38 +0200

    Fix: refactoring UPnP

--- app/lib/upnp.js
+++ app/lib/upnp.js
@@ -2,38 +2,43 @@ var upnp = require('nat-upnp');
 var async = require('async');
 var constants  = require('../lib/constants');
 var logger = require('../lib/logger')('upnp');
+var co = require('co');
+var Q = require('q');
 
-module.exports = function (localPort, remotePort, done) {
+module.exports = function (localPort, remotePort) {
   "use strict";
-  logger.info('Configuring UPnP...');
-  var client = upnp.createClient();
-  async.waterfall([
-    function (next) {
-      client.externalIp(function(err, ip) {
+  return co(function *() {
+    logger.info('UPnP: configuring...');
+    var client = upnp.createClient();
+    yield Q.nbind(client.externalIp, client)()
+      .catch(function(err){
         if (err && err.message == 'timeout') {
-          err = 'No UPnP gateway found: your node won\'t be reachable from the Internet. Use --noupnp option to avoid this message.';
+          throw 'No UPnP gateway found: your node won\'t be reachable from the Internet. Use --noupnp option to avoid this message.'
         }
-        next(err, ip);
+        throw err;
       });
-    },
-    function (ip, next) {
-      client.close();
-      // Update UPnP IGD every INTERVAL seconds
-      setInterval(async.apply(openPort, localPort, remotePort), 1000 * constants.NETWORK.UPNP.INTERVAL);
-      openPort(localPort, remotePort, next);
-    }
-  ], done);
+    client.close();
+    // Update UPnP IGD every INTERVAL seconds
+    setInterval(async.apply(openPort, localPort, remotePort), 1000 * constants.NETWORK.UPNP.INTERVAL);
+    openPort(localPort, remotePort);
+  });
 };
 
-function openPort (localPort, remotePort, done) {
+function openPort (localPort, remotePort) {
   "use strict";
-  var client = upnp.createClient();
-  client.portMapping({
-    public: parseInt(remotePort),
-    private: parseInt(localPort),
-    ttl: constants.NETWORK.UPNP.TTL
-  }, function(err) {
-    client.close();
-    return done && done(err);
+  return Q.Promise(function(resolve, reject){
+    logger.info('UPnP: mapping external port %s to local %s...', remotePort, localPort);
+    var client = upnp.createClient();
+    client.portMapping({
+      'public': parseInt(remotePort),
+      'private': parseInt(localPort),
+      'ttl': constants.NETWORK.UPNP.TTL
+    }, function(err) {
+      client.close();
+      if (err) {
+        return reject(err);
+      }
+      resolve();
+    });
   });
 }

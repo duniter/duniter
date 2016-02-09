bbf3d88e9936e6a971b7810bf224df9c70d2ae00
cgeek <cem.moreau@gmail.com>
2016-02-09 15:17:00 +0100

    Fix #68 Added txunlocker

---
+++ app/lib/txunlock.js
@@ -0,0 +1,66 @@
+"use strict";
+
+let Parser = require("jison").Parser;
+let ucp = require('./ucp');
+let crypto = require('./crypto');
+
+let grammar = {
+  "lex": {
+    "rules": [
+      ["\\s+",                    "/* skip whitespace */"],
+      ["\\&\\&",                  "return 'AND';"],
+      ["\\|\\|",                  "return 'OR';"],
+      ["\\(",                     "return '(';"],
+      ["\\)",                     "return ')';"],
+      ["[0-9A-Za-z]{40,64}",      "return 'PARAMETER';"],
+      ["SIG",                     "return 'SIG';"],
+      ["XHX",                     "return 'XHX';"],
+      ["$",                       "return 'EOF';"]
+    ]
+  },
+
+  "operators": [
+    ["left", "AND", "OR"]
+  ],
+
+  "bnf": {
+    "expressions" :[
+      [ "e EOF",   "return $1;"  ]
+    ],
+
+    "e" :[
+      [ "e AND e", "$$ = $1 && $3;" ],
+      [ "e OR e",  "$$ = $1 || $3;" ],
+      [ "SIG ( e )","$$ = yy.sig($3);"],
+      [ "XHX ( e )","$$ = yy.xHx($3);"],
+      [ "PARAMETER", "$$ = $1;" ],
+      [ "( e )",   "$$ = $2;" ]
+    ]
+  }
+};
+
+let logger = require('../lib/logger')('unlock');
+
+module.exports = function unlock(conditionsStr, executions) {
+
+  let parser = new Parser(grammar);
+
+  parser.yy = {
+    i: 0,
+    sig: function (pubkey) {
+      let sigParam = executions[this.i++];
+      return pubkey === sigParam.pubkey && sigParam.sigOK;
+    },
+    xHx: function(hash) {
+      let xhxParam = executions[this.i++];
+      return ucp.format.hashf(xhxParam) === hash;
+    }
+  };
+
+  try {
+    return parser.parse(conditionsStr);
+  } catch(e) {
+    logger.error(e);
+    return false;
+  }
+};

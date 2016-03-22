a044f331e58076aa9e1d3fa09880ea5058b1ef67
cgeek <cem.moreau@gmail.com>
2016-03-22 19:07:21 +0100

    Fix: windows release was not put on the right github release

--- release.sh
+++ release.sh
@@ -10,7 +10,7 @@ if [[ $2 =~ ^[0-9]+.[0-9]+.[0-9]+((a|b)[0-9]+)?$ ]]; then
       # Change the version in package.json and test file
       sed -i "s/version\": \"$current/version\": \"$2/g" package.json
       sed -i "s/version').equal('.*/version').equal('$2');/g" test/integration/branches.js
-      sed -i "s/ release: .*/ release: $2/g" appveyor.yml
+      sed -i "s/ release: .*/ release: v$2/g" appveyor.yml
       if [[ "$1" =~ ^rel$ ]]; then
         # This is RELEASE: change the version in public installer + add the RELEASE flag
         sed -i "s/echo \"v.*\"/echo \"v$2\"/g" install.sh

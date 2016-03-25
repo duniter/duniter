4bbf75bceed52bfe3a168582a3b1f740bcb543b2
cgeek <cem.moreau@gmail.com>
2016-03-25 14:29:06 +0100

    Release script: add versions to GUI title bar

--- release.sh
+++ release.sh
@@ -11,6 +11,12 @@ if [[ $2 =~ ^[0-9]+.[0-9]+.[0-9]+((a|b)[0-9]+)?$ ]]; then
       sed -i "s/version\": \"$current/version\": \"$2/g" package.json
       sed -i "s/version').equal('.*/version').equal('$2');/g" test/integration/branches.js
       sed -i "s/ release: .*/ release: v$2/g" appveyor.yml
+
+      # GUI containers version
+      sed -i "s/name\": .*/name\": \"v$2\",/g" gui/package.json
+      sed -i "s/<title>Duniter<\/title>/<title>Duniter v$2<\/title>/g" gui/index.html
+
+      # (pre)-release management
       if [[ "$1" =~ ^rel$ ]]; then
         # This is RELEASE: change the version in public installer + add the RELEASE flag
         sed -i "s/echo \"v.*\"/echo \"v$2\"/g" install.sh
@@ -30,10 +36,10 @@ if [[ $2 =~ ^[0-9]+.[0-9]+.[0-9]+((a|b)[0-9]+)?$ ]]; then
   git reset HEAD
   case "$1" in
     rel)
-      git add package.json .travis.yml appveyor.yml test/integration/branches.js install.sh
+      git add package.json .travis.yml appveyor.yml test/integration/branches.js gui/package.json gui/index.html install.sh
       ;;
     pre)
-      git add package.json .travis.yml appveyor.yml test/integration/branches.js
+      git add package.json .travis.yml appveyor.yml test/integration/branches.js gui/package.json gui/index.html
       ;;
   esac
   git commit -m "v$2"

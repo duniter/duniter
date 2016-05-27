6d51f42d2cfdaff74d2c0eca584ba5726be2486f
cgeek <cem.moreau@gmail.com>
2016-05-27 11:37:11 +0200

    Fixing #454 Currently used ARM script was not under git

---
+++ ci/arm/make_arm_deb_package.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+ARCH=$1
+DUNITER_VER=$2
+NVER="v5.9.1"
+DUNITER_DEB_VER=" $DUNITER_VER"
+echo "$ARCH"
+echo "$NVER"
+echo "$DUNITER_VER"
+echo "$DUNITER_DEB_VER"
+
+echo "Downloading Nodejs"
+wget http://nodejs.org/dist/${NVER}/node-${NVER}-linux-${ARCH}.tar.gz
+echo "Extracting Nodejs"
+tar xzf node-${NVER}-linux-${ARCH}.tar.gz
+mv node-${NVER}-linux-${ARCH} node
+rm node-${NVER}-linux-${ARCH}.tar.gz
+
+echo "npm install"
+node/bin/npm install
+node/bin/npm prune --production
+SRC=`pwd`
+echo $SRC
+cd ..
+mkdir -p duniter_release/sources
+cp -R ${SRC}/* duniter_release/sources/
+mv duniter_release/sources/node duniter_release/
+rm -Rf duniter_release/sources/ui/node_modules
+rm -Rf duniter_release/sources/ui/bower_components
+
+# Creating DEB packaging
+mv duniter_release/sources/ci/travis/debian duniter-${ARCH}
+mkdir -p duniter-${ARCH}/opt/duniter/
+chmod 755 duniter-${ARCH}/DEBIAN/post*
+chmod 755 duniter-${ARCH}/DEBIAN/pre*
+sed -i "s/Version:.*/Version:$DUNITER_DEB_VER/g" duniter-${ARCH}/DEBIAN/control
+cd duniter_release/sources
+pwd
+rm -Rf .git
+echo "Zipping..."
+zip -qr ../duniter-desktop.nw *
+cd ../..
+mv duniter_release/duniter-desktop.nw duniter-${ARCH}/opt/duniter/
+mv duniter_release/node duniter-${ARCH}/opt/duniter/sources/
+echo "Making deb package"
+fakeroot dpkg-deb --build duniter-${ARCH}
+mv duniter-${ARCH}.deb duniter-v${DUNITER_VER}-linux-${ARCH}.deb
+echo "Uploading release..."
+./github-release upload -u duniter -r duniter --tag v${DUNITER_VER} --name duniter-v${DUNITER_VER}-linux-${ARCH}.deb --file ./duniter-v${DUNITER_VER}-linux-${ARCH}.deb

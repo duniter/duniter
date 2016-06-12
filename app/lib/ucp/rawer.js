0a9493a94bc7b4dc4c76058c3404e32d0c54c12a
inso <insomniak.fr@gmaiL.com>
2016-06-12 22:51:45 +0200

    Remove that

--- app/lib/ucp/rawer.js
+++ app/lib/ucp/rawer.js
@@ -4,9 +4,7 @@ let constants = require('../constants');
 
 module.exports = new function() {
 
-  let that = this;
-
-  this.getOfficialIdentity = function (json) {
+  this.getOfficialIdentity = (json) => {
     let raw = "";
     raw += "Version: " + (json.version || constants.DOCUMENTS_VERSION) + "\n";
     raw += "Type: Identity\n";
@@ -20,7 +18,7 @@ module.exports = new function() {
     return dos2unix(raw);
   };
 
-  this.getOfficialCertification = function (json) {
+  this.getOfficialCertification = (json) => {
     let raw = getNormalHeader('Certification', json);
     raw += "IdtyIssuer: " + json.idty_issuer + '\n';
     raw += "IdtyUniqueID: " + json.idty_uid + '\n';
@@ -33,7 +31,7 @@ module.exports = new function() {
     return dos2unix(raw);
   };
 
-  this.getOfficialRevocation = function (json) {
+  this.getOfficialRevocation = (json) => {
     let raw = getNormalHeader('Revocation', json);
     raw += "IdtyUniqueID: " + json.uid + '\n';
     raw += "IdtyTimestamp: " + json.buid + '\n';
@@ -44,7 +42,7 @@ module.exports = new function() {
     return dos2unix(raw);
   };
 
-  this.getPeerWithoutSignature = function (json) {
+  this.getPeerWithoutSignature = (json) => {
     let raw = "";
     raw += "Version: " + (json.version || constants.DOCUMENTS_VERSION) + "\n";
     raw += "Type: Peer\n";
@@ -52,17 +50,17 @@ module.exports = new function() {
     raw += "PublicKey: " + json.pubkey + "\n";
     raw += "Block: " + json.block + "\n";
     raw += "Endpoints:" + "\n";
-    (json.endpoints || []).forEach(function(ep){
+    (json.endpoints || []).forEach((ep) => {
       raw += ep + "\n";
     });
     return dos2unix(raw);
   };
 
-  this.getPeer = function (json) {
-    return dos2unix(signed(that.getPeerWithoutSignature(json), json));
+  this.getPeer = (json) => {
+    return dos2unix(signed(this.getPeerWithoutSignature(json), json));
   };
 
-  this.getMembershipWithoutSignature = function (json) {
+  this.getMembershipWithoutSignature = (json) => {
     let raw = getNormalHeader('Membership', json);
     raw += "Block: " + json.block + "\n";
     raw += "Membership: " + json.membership + "\n";
@@ -73,11 +71,11 @@ module.exports = new function() {
     return dos2unix(raw);
   };
 
-  this.getMembership = function (json) {
-    return dos2unix(signed(that.getMembershipWithoutSignature(json), json));
+  this.getMembership = (json) => {
+    return dos2unix(signed(this.getMembershipWithoutSignature(json), json));
   };
 
-  this.getBlockInnerPart = function (json) {
+  this.getBlockInnerPart = (json) => {
     let raw = "";
     raw += "Version: " + (json.version || constants.DOCUMENTS_VERSION) + "\n";
     raw += "Type: Block\n";
@@ -128,99 +126,99 @@ module.exports = new function() {
     }
     raw += "Transactions:\n";
     for(let i = 0; i < json.transactions.length; i++){
-      raw += json.transactions[i].raw || that.getCompactTransaction(json.transactions[i]);
+      raw += json.transactions[i].raw || this.getCompactTransaction(json.transactions[i]);
     }
     return dos2unix(raw);
   };
 
-  this.getBlockWithInnerHashAndNonce = function (json) {
-    let raw = that.getBlockInnerPart(json);
+  this.getBlockWithInnerHashAndNonce = (json) => {
+    let raw = this.getBlockInnerPart(json);
     raw += "InnerHash: " + json.inner_hash + "\n";
     raw += "Nonce: " + json.nonce + "\n";
     return dos2unix(raw);
   };
 
-  this.getBlockInnerHashAndNonce = function (json) {
+  this.getBlockInnerHashAndNonce = (json) => {
     let raw = "" +
       "InnerHash: " + json.inner_hash + "\n" +
       "Nonce: " + json.nonce + "\n";
     return dos2unix(raw);
   };
 
-  this.getBlockInnerHashAndNonceWithSignature = function (json) {
+  this.getBlockInnerHashAndNonceWithSignature = (json) => {
     let raw = "" +
       "InnerHash: " + json.inner_hash + "\n" +
       "Nonce: " + json.nonce + "\n";
     return dos2unix(signed(raw, json));
   };
 
-  this.getBlock = function (json) {
-    return dos2unix(signed(that.getBlockWithInnerHashAndNonce(json), json));
+  this.getBlock = (json) => {
+    return dos2unix(signed(this.getBlockWithInnerHashAndNonce(json), json));
   };
 
-  this.getTransaction = function (json) {
+  this.getTransaction = (json) => {
     let raw = "";
     raw += "Version: " + (json.version || constants.DOCUMENTS_VERSION) + "\n";
     raw += "Type: Transaction\n";
     raw += "Currency: " + json.currency + "\n";
     raw += "Locktime: " + json.locktime + "\n";
     raw += "Issuers:\n";
-    (json.issuers || []).forEach(function (issuer) {
+    (json.issuers || []).forEach((issuer) => {
       raw += issuer + '\n';
     });
     raw += "Inputs:\n";
-    (json.inputs || []).forEach(function (input) {
+    (json.inputs || []).forEach((input) => {
       raw += input + '\n';
     });
     raw += "Unlocks:\n";
-    (json.unlocks || []).forEach(function (input) {
+    (json.unlocks || []).forEach((input) => {
       raw += input + '\n';
     });
     raw += "Outputs:\n";
-    (json.outputs || []).forEach(function (output) {
+    (json.outputs || []).forEach((output) => {
       raw += output + '\n';
     });
     raw += "Comment: " + (json.comment || "") + "\n";
-    (json.signatures || []).forEach(function (signature) {
+    (json.signatures || []).forEach((signature) => {
       raw += signature + '\n';
     });
     return dos2unix(raw);
   };
 
-  this.getCompactTransaction = function (json) {
+  this.getCompactTransaction = (json) => {
     let issuers = (json.issuers || json.signatories);
     let raw = ["TX", json.version, issuers.length, json.inputs.length, json.unlocks.length, json.outputs.length, json.comment ? 1 : 0, json.locktime || 0].join(':') + '\n';
-    (issuers || []).forEach(function (issuer) {
+    (issuers || []).forEach((issuer) => {
       raw += issuer + '\n';
     });
-    (json.inputs || []).forEach(function (input) {
+    (json.inputs || []).forEach((input) => {
       raw += input + '\n';
     });
-    (json.unlocks || []).forEach(function (input) {
+    (json.unlocks || []).forEach((input) => {
       raw += input + '\n';
     });
-    (json.outputs || []).forEach(function (output) {
+    (json.outputs || []).forEach((output) => {
       raw += output + '\n';
     });
     if (json.comment)
       raw += json.comment + '\n';
-    (json.signatures || []).forEach(function (signature) {
+    (json.signatures || []).forEach((signature) => {
       raw += signature + '\n';
     });
     return dos2unix(raw);
   };
 
-  function getNormalHeader(doctype, json) {
+  let getNormalHeader = (doctype, json) => {
     let raw = "";
     raw += "Version: " + (json.version || constants.DOCUMENTS_VERSION) + "\n";
     raw += "Type: " + doctype + "\n";
     raw += "Currency: " + json.currency + "\n";
     raw += "Issuer: " + json.issuer + "\n";
     return raw;
-  }
+  };
 
-  function signed (raw, json) {
+  let signed = (raw, json) => {
     raw += json.signature + '\n';
     return raw;
-  }
+  };
 };

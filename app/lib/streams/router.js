bfe7acfe2351cbe06ecd4c9c8d8f1fa7af5e9bf9
cgeek <cem.moreau@gmail.com>
2016-11-12 12:44:04 +0100

    Fix #647 Too many logs "A newer peer document is available"

--- app/lib/streams/router.js
+++ app/lib/streams/router.js
@@ -57,7 +57,11 @@ function Router (PeeringService, conf, dal) {
           yield PeeringService.handleNewerPeer(obj.peer);
         }
       } catch (e) {
-        logger.error("Routing error: %s", e && (e.stack || e.message || e));
+        if (e && e.uerr && e.uerr.ucode == constants.ERRORS.NEWER_PEER_DOCUMENT_AVAILABLE.uerr.ucode) {
+          logger.info('Newer peer document available on the network for local node');
+        } else {
+          logger.error("Routing error: %s", e && (e.stack || e.message || e));
+        }
       }
       done && done();
     });

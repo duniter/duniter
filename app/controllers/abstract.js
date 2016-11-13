5d767dc69473cae2c0ad28e362d4c73af5463b28
cgeek <cem.moreau@gmail.com>
2016-11-13 16:02:01 +0100

    Fixing #700 Written.json is not a function

--- app/controllers/abstract.js
+++ app/controllers/abstract.js
@@ -5,10 +5,18 @@ const dos2unix = require('../lib/system/dos2unix');
 
 module.exports = function AbstractController (server) {
 
+  const logger = require('../lib/logger')('abstractController');
+
   this.pushEntity = (req, rawer, type) => co(function *() {
     let rawDocument = rawer(req);
     rawDocument = dos2unix(rawDocument);
     const written = yield server.writeRaw(rawDocument, type);
-    return written.json();
+    try {
+      return written.json();
+    } catch (e) {
+      logger.error('Written:', written);
+      logger.error(e);
+      throw e;
+    }
   });
 };

e2c4d55c44fef4e92b15a9c918bca9e0ee16f10a
cgeek <cem.moreau@gmail.com>
2017-07-11 11:39:40 +0200

    [enh] #875 MISC indexer should extend IndexedBlockchain

--- app/lib/blockchain/miscIndexedBlockchain.js
+++ app/lib/blockchain/miscIndexedBlockchain.js
@@ -4,33 +4,35 @@ const co = require('co')
 const IndexedBlockchain = require('./indexedBlockchain')
 const SQLIndex = require('./sqlIndex')
 
-module.exports = function (blockchainStorage, mindexDAL, iindexDAL, sindexDAL, cindexDAL) {
+module.exports = class MiscIndexedBlockchain extends IndexedBlockchain {
 
-  return new IndexedBlockchain(blockchainStorage, SQLIndex(null, {
-    m_index: { handler: mindexDAL },
-    i_index: { handler: iindexDAL },
-    s_index: {
-      handler: sindexDAL,
-      findTrimable: (maxNumber) => sindexDAL.query('SELECT * FROM s_index WHERE consumed AND writtenOn < ?', [maxNumber])
-    },
-    c_index: {
-      handler: cindexDAL,
-      findTrimable: (maxNumber) => cindexDAL.query('SELECT * FROM c_index WHERE expired_on > 0 AND writtenOn < ?', [maxNumber])
-    }
-  }), 'writtenOn', {
-    m_index: {
-      pk: ['pub']
-    },
-    i_index: {
-      pk: ['pub']
-    },
-    s_index: {
-      pk: ['identifier', 'pos'],
-      remove: 'consumed'
-    },
-    c_index: {
-      pk: ['issuer', 'receiver', 'created_on'],
-      remove: 'expired_on'
-    }
-  })
+  constructor(blockchainStorage, mindexDAL, iindexDAL, sindexDAL, cindexDAL) {
+    super(blockchainStorage, SQLIndex(null, {
+      m_index: { handler: mindexDAL },
+      i_index: { handler: iindexDAL },
+      s_index: {
+        handler: sindexDAL,
+        findTrimable: (maxNumber) => sindexDAL.query('SELECT * FROM s_index WHERE consumed AND writtenOn < ?', [maxNumber])
+      },
+      c_index: {
+        handler: cindexDAL,
+        findTrimable: (maxNumber) => cindexDAL.query('SELECT * FROM c_index WHERE expired_on > 0 AND writtenOn < ?', [maxNumber])
+      }
+    }), 'writtenOn', {
+      m_index: {
+        pk: ['pub']
+      },
+      i_index: {
+        pk: ['pub']
+      },
+      s_index: {
+        pk: ['identifier', 'pos'],
+        remove: 'consumed'
+      },
+      c_index: {
+        pk: ['issuer', 'receiver', 'created_on'],
+        remove: 'expired_on'
+      }
+    })
+  }
 }

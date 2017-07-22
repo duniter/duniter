d795202e93688f28961bdf7c83df48ab4edcfcda
cgeek <cem.moreau@gmail.com>
2017-07-22 15:31:02 +0200

    [fix] #1041 Make tests pass

--- test/dal/dal.js
+++ test/dal/dal.js
@@ -103,7 +103,7 @@ describe("DAL", function(){
   it('should have DB version 21', () => co(function *() {
     let version = yield fileDAL.getDBVersion();
     should.exist(version);
-    version.should.equal(24);
+    version.should.equal(25);
   }));
 
   it('should have no peer in a first time', function(){

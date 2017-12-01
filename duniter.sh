362688ee5c887bcc99f63eeee4a0260c400dcd0d
Moul <moul@moul.re>
2017-12-01 21:08:13 +0100

    [mod] Allow to run Node.js v8.

--- duniter.sh
+++ duniter.sh
@@ -35,8 +35,8 @@ duniter() {
 
 	VERSION=`$NODE -v`
 
-	if [[ $VERSION != v6* ]]; then
-	  echo "$NODE v6 is required";
+	if [[ $VERSION != v6.* && $VERSION != v8.* ]]; then
+	  echo "$NODE v6 or v8 is required";
 	else
 
 	  # Calls duniter JS command

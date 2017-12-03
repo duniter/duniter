bffadce1ab719111917d09aae9d38f54dfb13637
cgeek <cem.moreau@gmail.com>
2017-12-03 17:43:28 +0100

    [enh] #689 Allow to make a general build locally

--- release/new_prerelease.sh
+++ release/new_prerelease.sh
@@ -3,35 +3,25 @@
 TAG="v$1"
 TOKEN=`cat $HOME/.config/duniter/.github`
 ARCH=`uname -m`
+ASSETS=
 # Check that the tag exists remotely
 
-if [[ -z $TAG ]]; then
-  echo "Wrong call to the command, syntax is:"
-  echo ""
-  echo "  new_prerelease.sh <tag>"
-  echo ""
-  echo "Examples:"
-  echo ""
-  echo "  new_prerelease.sh 1.2.3"
-  echo "  new_prerelease.sh 1.4.0"
-  echo "  new_prerelease.sh 1.4.1"
-  echo ""
-  exit 1
+if [[ ! -z $1 ]]; then
+  echo "Checking that $TAG has been pushed to 'origin'..."
+
+  REMOTE_TAG=`git ls-remote --tags origin | grep -Fo "$TAG"`
+
+  if [[ -z $REMOTE_TAG ]]; then
+    echo "The '$TAG' tag does not exist on 'origin' repository. Use command ./release/new_version.sh to create a new version and use 'git push origin --tags' to share the tag."
+    exit 2
+  fi
+
+  echo "Remote tag: $REMOTE_TAG"
+
+  echo "Creating the pre-release..."
+  ASSETS=`node ./release/scripts/create-release.js $TOKEN $TAG create`
 fi
 
-echo "Checking that $TAG has been pushed to 'origin'..."
-
-REMOTE_TAG=`git ls-remote --tags origin | grep -Fo "$TAG"`
-
-if [[ -z $REMOTE_TAG ]]; then
-  echo "The '$TAG' tag does not exist on 'origin' repository. Use command ./release/new_version.sh to create a new version and use 'git push origin --tags' to share the tag."
-  exit 2
-fi
-
-echo "Remote tag: $REMOTE_TAG"
-
-echo "Creating the pre-release..."
-ASSETS=`node ./release/scripts/create-release.js $TOKEN $TAG create`
 EXPECTED_ASSETS="duniter-desktop-$TAG-linux-x64.deb
 duniter-desktop-$TAG-linux-x64.tar.gz
 duniter-server-$TAG-linux-x64.deb
@@ -47,8 +37,10 @@ for asset in $EXPECTED_ASSETS; do
       if [[ $ARCH == "x86_64" ]]; then
         echo "Starting Debian build..."
         ./release/scripts/build.sh make deb $TAG
-        DEB_PATH="$PWD/release/arch/debian/$asset"
-        node ./release/scripts/upload-release.js $TOKEN $TAG $DEB_PATH
+        if [[ ! -z $1 ]]; then
+          DEB_PATH="$PWD/release/arch/debian/$asset"
+          node ./release/scripts/upload-release.js $TOKEN $TAG $DEB_PATH
+        fi
       else
         echo "This computer cannot build this asset, required architecture is 'x86_64'. Skipping."
       fi
@@ -59,8 +51,10 @@ for asset in $EXPECTED_ASSETS; do
       if [[ $ARCH == "x86_64" ]]; then
         echo "Starting Windows build..."
         ./release/scripts/build.sh make win $TAG
-        WIN_PATH="$PWD/release/arch/windows/$asset"
-        node ./release/scripts/upload-release.js $TOKEN $TAG $WIN_PATH
+        if [[ ! -z $1 ]]; then
+          WIN_PATH="$PWD/release/arch/windows/$asset"
+          node ./release/scripts/upload-release.js $TOKEN $TAG $WIN_PATH
+        fi
       else
         echo "This computer cannot build this asset, required architecture is 'x86_64'. Skipping."
       fi
@@ -71,8 +65,10 @@ for asset in $EXPECTED_ASSETS; do
       if [[ $ARCH == "armv7l" ]]; then
         echo "Starting ARM build..."
         ./release/scripts/build.sh make arm $TAG
-        ARM_PATH="$PWD/release/arch/arm/$asset"
-        node ./release/scripts/upload-release.js $TOKEN $TAG $ARM_PATH
+        if [[ ! -z $1 ]]; then
+          ARM_PATH="$PWD/release/arch/arm/$asset"
+          node ./release/scripts/upload-release.js $TOKEN $TAG $ARM_PATH
+        fi
       else
         echo "This computer cannot build this asset, required architecture is 'armv7l'. Skipping."
       fi

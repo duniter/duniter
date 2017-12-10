d7fed2fd46d679965caf42c3be6931f5dd66170d
librelois <elois@ifee.fr>
2017-12-10 17:09:44 +0100

    Revert "Merge branch 'better_cpu_adjustement' into '1.6'"
    This reverts commit 7df69dbd4744cfdde552acdd9597ddf039c37abb, reversing
    changes made to 45f6376f5e3a8ec27abf5426603792ae4bb81294.

--- app/modules/prover/lib/engine.ts
+++ app/modules/prover/lib/engine.ts
@@ -25,27 +25,32 @@ export class PowEngine {
     this.id = this.cluster.clusterId
   }
 
-  getNbWorkers() {
-    return this.cluster.nbWorkers
-  }
-
   forceInit() {
     return this.cluster.initCluster()
   }
 
   async prove(stuff:any) {
-
-    if (this.cluster.hasProofPending) {
-      await this.cluster.cancelWork()
-    }
-    return await this.cluster.proveByWorkers(stuff)
-  }
+    
+        if (this.cluster.hasProofPending) {
+          await this.cluster.cancelWork()
+        }
+    
+        const cpus = os.cpus()
+    
+        if (os.arch().match(/arm/) || cpus[0].model.match(/Atom/)) {
+          stuff.newPoW.conf.nbCores /= 2; // Make sure that only once each physical core is used (for Hyperthreading).
+        }
+        return await this.cluster.proveByWorkers(stuff)
+  }    
 
   cancel() {
     return this.cluster.cancelWork()
   }
 
   setConf(value:any) {
+    if (os.arch().match(/arm/) && value.cpu !== undefined) {
+      value.cpu /= 2; // Don't know exactly why is ARM so much saturated by PoW, so let's divide by 2
+    }
     return this.cluster.changeConf(value)
   }
 

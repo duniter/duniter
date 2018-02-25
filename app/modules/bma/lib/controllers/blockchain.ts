8f292c58ef27e0e1321e60ac0bacfe02840f7ddc
Cédric Moreau <cem.moreau@gmail.com>
2018-02-25 11:35:35 +0000

    Revert "Merge branch 'add-license-header' into 'add-license-header'"
    This reverts merge request !1250

--- app/modules/bma/lib/controllers/blockchain.ts
+++ app/modules/bma/lib/controllers/blockchain.ts
@@ -1,16 +1,3 @@
-// Source file from duniter: Crypto-currency software to manage libre currency such as Ğ1
-// Copyright (C) 2018  Cedric Moreau <cem.moreau@gmail.com>
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-
 "use strict";
 import {Server} from "../../../../../server"
 import {AbstractController} from "./AbstractController"

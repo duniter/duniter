3c910601e7c27b2088fdf058ab179db1ab15b6cb
cgeek <cem.moreau@gmail.com>
2019-04-03 13:17:00 +0200

    [enh] dump-ww: use file copy + semaphore to trigger the copy

--- app/modules/dump/wotwizard/wotwizard.dump.ts
+++ app/modules/dump/wotwizard/wotwizard.dump.ts
@@ -1,3 +1,4 @@
+import * as fs from "fs"
 import {Server} from "../../../../server"
 import {createExportStructure} from "./wotwizard.init.structure"
 import {WotWizardConstants} from "./wotwizard.constants"
@@ -5,11 +6,12 @@ import {addLegacyBlocks} from "./wotwizard.legacy.blocks"
 import {addNewBlocks} from "./wotwizard.new.blocks"
 import {deleteNonLegacy} from "./wotwizard.delete"
 import {copyMemPool} from "./wotwizard.copy.mempool"
+import {Directory} from "../../../lib/system/directory"
 
 export async function dumpWotWizard(server: Server) {
 
   // 1. Create dump structure if it does not exist
-  const wwDAL = await createExportStructure(WotWizardConstants.DB_NAME)
+  const wwDAL = await createExportStructure(WotWizardConstants.DB_NAME_0)
 
   // 2. Integrate legacy blocks (= non-forkable)
   await addLegacyBlocks(server, wwDAL)
@@ -22,4 +24,28 @@ export async function dumpWotWizard(server: Server) {
 
   // 5. Copy mempool
   await copyMemPool(server, wwDAL)
+
+  // 6. Close SQL connections
+  await Promise.all([
+    wwDAL.blockDao,
+    wwDAL.iindexDao,
+    wwDAL.idtyDao,
+    wwDAL.certDao,
+    wwDAL.msDao,
+  ].map(dao => dao.close()))
+
+  // 7. Copy
+  let lastCopyIsOldEnough = false
+  const updatingFile = Directory.GET_FILE_PATH(WotWizardConstants.FILE_UPDATING)
+  if (fs.existsSync(updatingFile)) {
+    const content = parseInt(fs.readFileSync(updatingFile, 'utf8'))
+    lastCopyIsOldEnough = Date.now() - content > WotWizardConstants.DELAY_FOR_UPDATING
+  } else {
+    // Never done
+    lastCopyIsOldEnough = true
+  }
+  if (lastCopyIsOldEnough) {
+    fs.copyFileSync(Directory.GET_FILE_PATH(WotWizardConstants.DB_NAME_0), Directory.GET_FILE_PATH(WotWizardConstants.DB_NAME))
+    fs.writeFileSync(updatingFile, Date.now())
+  }
 }

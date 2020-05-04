c9b81ebcbf566787c3bc1150a770fc382db663a3
librelois <elois@ifee.fr>
2020-05-04 21:37:56 +0200

    [build] allow version format x.y.z-beta

--- release/new_version.sh
+++ release/new_version.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ### Releasing
-current=`grep -P "version\": \"\d+.\d+.\d+(\w*)" package.json | grep -oP "\d+.\d+.\d+(\w*)"`
+current=`grep -P "version\": \"\d+.\d+.\d+(-\w*)" package.json | grep -oP "\d+.\d+.\d+(-\w*)"`
 echo "Current version: $current"
 
-if [[ $1 =~ ^[0-9]+.[0-9]+.[0-9]+((a|b)[0-9]+)?$ ]]; then
+if [[ $1 =~ ^[0-9]+.[0-9]+.[0-9]+(-(alpha|beta|rc)[0-9]*)?$ ]]; then
   echo "Changing to version: $1"
   # Change the version in package.json and test file
   sed -i "s/version\": .*/version\": \"$1\",/g" package.json

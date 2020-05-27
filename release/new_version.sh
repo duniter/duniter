970615ae57c5b6fcd208189a78f0ec468fffcfa6
librelois <elois@ifee.fr>
2020-05-27 16:00:53 +0200

    [build] upgrade duniter version on package-lock

--- release/new_version.sh
+++ release/new_version.sh
@@ -6,20 +6,23 @@ echo "Current version: $current"
 
 if [[ $1 =~ ^[0-9]+.[0-9]+.[0-9]+(-(alpha|beta|rc)[0-9]*)?$ ]]; then
   echo "Changing to version: $1"
-  # Change the version in package.json and test file
-  sed -i "s/version\": .*/version\": \"$1\",/g" package.json
-  sed -i "s/Version: .*/Version: $1/g" release/extra/debian/package/DEBIAN/control
+  # Change the version in package.json and package-lock.json
+  sed -i "s/version\": .*/version\": \"$1\",/" package.json
+  sed -i "3 s/version\": .*/version\": \"$1\",/" package-lock.json
+
+  # Debian file
+  sed -i "s/Version: .*/Version: $1/" release/extra/debian/package/DEBIAN/control
 
   # Duniter.iss (Windows installer)
-  sed -i "s/define MyAppVerStr.*/define MyAppVerStr \"v$1\"/g" release/arch/windows/duniter.iss
+  sed -i "s/define MyAppVerStr.*/define MyAppVerStr \"v$1\"/" release/arch/windows/duniter.iss
 
   # GUI containers version
-  sed -i "s/title\": .*/title\": \"v$1\",/g" package.json
-  sed -i "s/<title>Duniter.*<\/title>/<title>Duniter $1<\/title>/g" gui/index.html
+  sed -i "s/title\": .*/title\": \"v$1\",/" package.json
+  sed -i "s/<title>Duniter.*<\/title>/<title>Duniter $1<\/title>/" gui/index.html
 
   # Commit
   git reset HEAD
-  git add package.json gui/index.html release/extra/debian/package/DEBIAN/control release/arch/windows/duniter.iss
+  git add package.json package-lock.json gui/index.html release/extra/debian/package/DEBIAN/control release/arch/windows/duniter.iss
   git commit -m "v$1"
   git tag "v$1"
 else

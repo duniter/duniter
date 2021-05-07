445cc2d96ec2ed468fb5c452f0043d5a55bff425
librelois <c@elo.tf>
2021-05-07 16:12:23 +0200

    fix(docker): modules conf must be saved on /etc/duniter

--- release/docker/duniter.sh
+++ release/docker/duniter.sh
@@ -5,10 +5,13 @@ if [[ -z ${1} ]]; then
 	set -- direct_webstart
 fi
 
-# Options
+# Define DUNITER_WEB_UI_HOST
 DUNITER_WEB_UI_HOST="0.0.0.0"
 export DUNITER_WEB_UI_HOST
 
+# Define DUNITER_CONF_HOME
+DUNITER_CONF_HOME="/etc/duniter"
+export DUNITER_CONF_HOME
 
 # Key file found
 if [[ -f /etc/duniter/key.yml ]]; then
